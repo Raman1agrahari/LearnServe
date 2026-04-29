5306229580498cd9a1ec678e96281af0a462008e
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2026-04-29 12:26:36 +0530

    ip

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 3000;
 
 
  connectDb().then( () => {
-  app.listen(PORT, () => {
+  app.listen(PORT,"0.0.0.0", () => {
     console.log(`server is running at port: ${PORT}`);
   });
  });
